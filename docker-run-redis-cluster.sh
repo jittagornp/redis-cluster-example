939f9fa75cdf679981389a5b75b7053e45573559
jittagorn pitakmetagoon <jittagornp@gmail.com>
2020-05-23 22:23:08 +0700

    Update docker-run-redis-cluster.sh

--- docker-run-redis-cluster.sh
+++ docker-run-redis-cluster.sh
@@ -6,4 +6,4 @@ server3=127.0.0.1
 server4=127.0.0.1 
 server5=127.0.0.1 
 server6=127.0.0.1 
-redis-cli --cluster create $server1:6379 $server2:6379 $server3:6379 $server4:6379 $server5:6379 $server6:6379 --cluster-replicas 1
+docker run -i --rm --name=redis-cluster redis redis-cli -a password --cluster create $server1:6379 $server2:6379 $server3:6379 $server4:6379 $server5:6379 $server6:6379 --cluster-replicas 1
