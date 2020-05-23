b23ec510a1405da980cc8464fcf5f8a9263c0d82
jittagorn pitakmetagoon <jittagornp@gmail.com>
2020-05-23 21:17:57 +0700

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
