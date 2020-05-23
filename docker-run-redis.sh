48ae625a34dfd8c07b7a5a59d24d4c92d4e68a3f
jittagorn pitakmetagoon <jittagornp@gmail.com>
2020-05-23 23:12:09 +0700

    Update docker-run-redis.sh

--- docker-run-redis.sh
+++ docker-run-redis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name redis --restart=always -d -p 6379:6379 -v $(pwd)/redis.conf:/usr/local/etc/redis/redis.conf redis:latest redis-server /usr/local/etc/redis/redis.conf --requirepass password
+docker run --name redis --restart=always -d -p 6379:6379 --net=host -v $(pwd)/redis.conf:/usr/local/etc/redis/redis.conf redis:latest redis-server /usr/local/etc/redis/redis.conf --requirepass password
